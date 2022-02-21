2100cd2ae7ced28c8a3f9231877f6d6dea4153f2
GrayOrchid <jorajostart@bk.ru>
2022-02-21 18:23:02 +0300

    update

--- hover-board/i.js
+++ hover-board/i.js
@@ -1,5 +1,8 @@
 let $cont = document.querySelector('.cont')
 let squares = 401
+let $reset = document.querySelector('.reset')
+
+
 let colors = ['#E5F0FF','#5BFF62','#580BE4','#FE2F57','#F464FE','#00848C']
 for (let i = 0; i < squares; i++) {
 
@@ -11,22 +14,24 @@ $square.classList.add('square')
 $cont.appendChild($square)
 
 
+
 $square.addEventListener('mousemove',()=>{
  for (let i = 0; i < colors.length; i++) {
     let random =  Math.floor(Math.random() * i);
       $square.style.background = colors[random]
  $square.style.boxShadow = `0 0 20px ${colors[random]}`
+ $square.style.transform= 'rotate(360deg)'
+
  }
-
+ 
 })
 
-$square.addEventListener('mousemove',()=>{
-    setTimeout(() => {
-        $square.style.background = '#1d1d1d'
-        $square.style.boxShadow = `0 0 2px black`
-    }, 2000);
+$reset.addEventListener('click',()=>{
+    $square.style.background = '#1d1d1d'
+   $square.style.boxShadow = `0 0 2px black`
 })
+
+
 
 
 }
-
