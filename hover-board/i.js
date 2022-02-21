5d32c67c2c3a704b6b9c5bf75d19fb7cdd54f8d6
GrayOrchid <jorajostart@bk.ru>
2022-02-21 12:43:23 +0300

    0.2

--- hover-board/i.js
+++ hover-board/i.js
@@ -1,12 +1,32 @@
 let $cont = document.querySelector('.cont')
-let squares = 500
-
+let squares = 401
+let colors = ['#E5F0FF','#5BFF62','#580BE4','#FE2F57','#F464FE','#00848C']
 for (let i = 0; i < squares; i++) {
+
+
 let $square = document.createElement('div')
-// console.log($square);
+
 $square.classList.add('square')
 
 $cont.appendChild($square)
 
 
+$square.addEventListener('mousemove',()=>{
+ for (let i = 0; i < colors.length; i++) {
+    let random =  Math.floor(Math.random() * i);
+      $square.style.background = colors[random]
+ $square.style.boxShadow = `0 0 20px ${colors[random]}`
+ }
+
+})
+
+$square.addEventListener('mousemove',()=>{
+    setTimeout(() => {
+        $square.style.background = '#1d1d1d'
+        $square.style.boxShadow = `0 0 2px black`
+    }, 2000);
+})
+
+
 }
+
